abeeffa237bfcf9e928ecdf0c1e0a94521349cc5
kriti-a567 <kriti.a.64@gmail.com>
2026-02-18 09:26:38 -0800

    Cleaned both init files

--- src/uh2_aim2/__init__.py
+++ src/uh2_aim2/__init__.py
@@ -1,10 +1,1 @@
-"""UH2 AIM2 Behavioral Analysis Package."""
 
-from uh2_aim2.behavior_qc.behavior_qc import run_qc_pipeline
-
-__all__ = ["run_qc_pipeline"]
-
-
-def main() -> None:
-    """Run the behavioral QC pipeline."""
-    run_qc_pipeline()
